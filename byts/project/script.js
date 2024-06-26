2e9b6b2fb1e6432a4766bf9df2b25df4150f0b62
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-06-26 22:12:18 +0530

    update

---
+++ byts/project/script.js
@@ -0,0 +1,106 @@
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.info("my info");
+// console.warn("Warning sd");
+// console.error("Error");
+// console.table([0,1,2]);
+
+// document.getElementById("clg");
+// console.log(document.getElementsByClassName("city"));
+// document.querySelector
+
+
+// console.log("check only value");
+// console.log(45 == 45);
+// console.log("45" == "45");
+// console.log("45" == 45);
+// console.log( "check both datatype and value")
+// console.log(45 === 45)
+// console.log(45.0 === 45) //true
+// console.log(45.2 === 45) //false
+// console.log("45" === 45) //false
+// console.log("45" === "45")
+
+
+
+// console.log(typeof(5)) // number 
+// console.log(typeof('asma')) //no char only string
+// console.log(typeof("asma")) //string
+// console.log(typeof(asma)) //undefined
+// console.log(typeof(class myclass{})) // both class and func is function
+// console.log(typeof(function myfunc(){})) //function
+
+// OBJECTS
+// console.log(cat = {
+//     legs : 4,
+//     name : "kutty",
+// })
+// console.log(typeof(cat));
+// console.log(cat.name);
+// console.log(cat["name"]);
+// cat.name = "moew";
+// console.log(cat.name);
+
+// cat.color = "sandal";
+
+// console.log(cat);
+
+
+// console.log(`my name is "asma" `) //backtick
+// console.log('my name is "asma" ') // single
+// console.log(`my 'name' is "asma" `) // can use both single and double quotes
+// // backtick is used for string formatting 
+// a = 10 ;
+// b = 20 ;
+// console.log(a + b) ;
+// console.log(`The value of ${a} and ${b} is ${a+b}`) ;
+
+
+//ARRAYS
+// a = [1,2,3,4]
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[-1]); //cant do negative indexing it'll show undefined
+
+// console.log(a.push(50));
+// console.log(a);
+// console.log(a.pop());
+// console.log(a);
+
+
+// nested array
+// a=[[1,2],[3,4],[5,6]];
+// console.log(a[0]);
+// console.log(a[0][0]);
+
+// a=[
+//     {name:'A',height:150},
+//     {name:'B',height:155},
+//     {name:'C',height:160}
+// ]
+// console.log(a[0]);
+// console.log(a[0].name);
+
+
+//JSON
+// obj={
+//     address:{
+//         doorno:120,
+//         city:{
+//             district: 'salem',
+//             town: 'sankari',
+//             pincode:638056
+//         }
+//     }
+// };
+
+// console.log(obj.address.doorno);//120
+// console.log(obj.address.city.district);//salem
+// console.log(obj.address.city.town);//sankari
+// console.log(obj.address.city.pincode);//638056
+// console.log(obj.address.city.street); //undefined
+// console.log(obj.address.sity.pincode);//error ...because there is not sity object but then we  looking up for
+//                                  //another object in undefine..so error
+
+
